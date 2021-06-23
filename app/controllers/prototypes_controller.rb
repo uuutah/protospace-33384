eafe981ad8cd7f36d6c7eae0b0e62aa335dab1e7
uuutah <78704588+uuutah@users.noreply.github.com>
2021-06-23 18:33:00 +0900

    fin

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,58 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_edit, only: [:edit, :update, :destroy]
+
   def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save   #Prototype.create(prototype_params)
+      redirect_to prototypes_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_edit
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get 'prototypes/index'
   ## ルートパスにアクセスした時に、先ほど定義したprototypesコントローラーのindexアクションが呼ばれる
   root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  ## ユーザーのルーティングは設定するのか後で確認
+  resources :users, only: :show
+  
 end
